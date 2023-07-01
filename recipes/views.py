f66c5b38ca33a05e38c1d2aa4917759a05700cd3
ZPRAT0 <guprates@live.com>
2023-07-01 14:27:42 -0300

    Exemplo de template

--- recipes/views.py
+++ recipes/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def home(request):
-    text = "<h1>HOME</h1>"
-    return HttpResponse(text)
+    lista = [i for i in range(1, 10)]
+    return render(request, 'recipes/home.html', {'name': lista})
 
 
 def about(request):
     text = "<h1>ABOUT</h1>"
-    return HttpResponse(text)
+    return render(request, 'me-apague/temp.html')
 
 
 def privacy(request):
